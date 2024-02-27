30ec4f2e415fb930509f49a037a6535397eef21d
Bank <tdvd13450@gmail.com>
2024-02-27 19:34:06 +0700

    use undefind instead of ' ' at UserListPage.tsx

--- src/pages/user/UserListPage.tsx
+++ src/pages/user/UserListPage.tsx
@@ -74,7 +74,7 @@ function UserListPage() {
               <Col xs={12}>
                 <Button
                   size="large"
-                  style={{ width: isMobile ? '100%' : '' }}
+                  style={{ width: isMobile ? '100%' : undefined }}
                   onClick={() => onOpen()}
                 >
                   <Space>
@@ -90,11 +90,11 @@ function UserListPage() {
                   flexDirection: 'row',
                   justifyContent: 'end',
                 }}>
-                <Link to={'/user/create'} style={{ width: isMobile ? '100%' : '' }}>
+                <Link to={'/user/create'} style={{ width: isMobile ? '100%' : undefined }}>
                   <Button
                     size="large"
                     type="primary"
-                    style={{ width: isMobile ? '100%' : '' }}
+                    style={{ width: isMobile ? '100%' : undefined }}
                     className="btn-primary"
                   >
                     <Text style={{ color: 'white' }}>{t('transacButton.addStaff')}</Text>
